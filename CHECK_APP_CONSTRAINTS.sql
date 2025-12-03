a416441b43499f0cee066203a7a274d75e68f5d4
ahmeddarvish <159630333+ahmeddarvish@users.noreply.github.com>
2025-12-03 14:17:59 +0300

    chore: Project cleanup - moved docs and sql files, deleted temp scripts

--- CHECK_APP_CONSTRAINTS.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-    conname as constraint_name, 
-    pg_get_constraintdef(c.oid) as constraint_definition
-FROM pg_constraint c 
-JOIN pg_namespace n ON n.oid = c.connamespace 
-WHERE n.nspname = 'public' 
-AND conrelid = 'applications'::regclass;
--- CHECK_COLUMN.sql
+++
@@ -1,4 +0,0 @@
--- Check if acceptance_letter_url exists in applications table
-SELECT column_name, data_type 
-FROM information_schema.columns 
-WHERE table_name = 'applications' AND column_name = 'acceptance_letter_url';
