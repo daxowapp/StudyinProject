cf121e263317462e7a9b35b80d6ead41317a2a72
ahmeddarvish <159630333+ahmeddarvish@users.noreply.github.com>
2025-12-29 14:34:58 +0300

    feat: add meta pixel script

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,6 +1,7 @@
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
 import { notFound } from 'next/navigation';
+import Script from 'next/script';
 import { routing } from '@/i18n/routing';
 import { Toaster } from "@/components/ui/sonner";
 import { LoadingBar } from "@/components/ui/loading-bar";
@@ -150,6 +151,33 @@ export default async function LocaleLayout({
                         <Toaster />
                     </CurrencyProvider>
                 </NextIntlClientProvider>
+                <Script
+                    id="fb-pixel"
+                    strategy="afterInteractive"
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            !function(f,b,e,v,n,t,s)
+                            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                            n.queue=[];t=b.createElement(e);t.async=!0;
+                            t.src=v;s=b.getElementsByTagName(e)[0];
+                            s.parentNode.insertBefore(t,s)}(window, document,'script',
+                            'https://connect.facebook.net/en_US/fbevents.js');
+                            fbq('init', '1378542223283943');
+                            fbq('track', 'PageView');
+                        `,
+                    }}
+                />
+                <noscript>
+                    <img
+                        height="1"
+                        width="1"
+                        style={{ display: 'none' }}
+                        src="https://www.facebook.com/tr?id=1378542223283943&ev=PageView&noscript=1"
+                        alt=""
+                    />
+                </noscript>
             </body>
         </html>
     );
